531db8248679f38fd989cf2e194f585b9ab2c369
Bruce Liu <yiyubruceliu@gmail.com>
2021-11-07 23:26:39 +0200

    shit about surface area triangles

---
+++ common.js
@@ -0,0 +1,7 @@
+function getInput(id) {
+    return document.getElementById(id).value;
+}
+
+function showOutput(id, message) {
+    document.getElementById(id).innerText = message;
+}
---
+++ surfaceArea.js
@@ -0,0 +1,11 @@
+function calcSurfaceArea(base, height) {
+    console.log('testicle connected');
+    console.log('shit came in', base, height);
+
+    //process
+    const answer = 0.5 * base * height;
+    console.log(answer);
+
+    //output
+    return answer;
+}
